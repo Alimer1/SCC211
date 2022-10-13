922a1c561f67275de73b52e8909e121d016e1f05
Ali Omer Misirli <aliomermsrl@gmail.com>
2022-10-13 21:45:43 +0100

    Syncronized the OperationController but I am not satisfied since I don't think I full understand it

--- Week 1/OperationController.java
+++ Week 1/OperationController.java
@@ -1,6 +1,6 @@
 public class OperationController implements Runnable
 {
-
+    private Object lockA = new Object();
     private int counter = 0;
 
     public OperationController()
@@ -8,17 +8,17 @@ public class OperationController implements Runnable
 
     }
 
-    public void run()
+    public synchronized void run()
     {
         if(Thread.currentThread().getName().compareTo("add") == 0)
         {
             ++counter;
-            System.out.println((counter-1)+"+1="+counter);
+            System.out.println("Thread added 1. Counter is now = "+counter);
         }
         else
         {
             --counter;
-            System.out.println((counter+1)+"-1="+counter);
+            System.out.println("Thread removed 1. Counter is now = "+counter);
         }
     }
 }
